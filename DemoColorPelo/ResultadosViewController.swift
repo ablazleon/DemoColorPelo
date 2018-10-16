bae638094700fd51cfe6d6ee0e1150140efcebff
Adrian <adrian@Adrians-Mac.local>
2018-10-16 10:57:29 +0200

    2. Seg

--- DemoColorPelo/ResultadosViewController.swift
+++ DemoColorPelo/ResultadosViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class ResultadosViewController: UIViewController {
 
+    var tipoDeTio: String? // It can be null
+    
+    @IBOutlet weak var tioLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        tioLabel.text = tipoDeTio
     }
-
+    
+    @IBAction func HaciaAtras(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- DemoColorPelo/ViewController.swift
+++ DemoColorPelo/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tioSegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Quiero pasar la inforamcion del segemented control a la otra.
+    // Sender is what you clicked
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Si el segue es el que quiero
+        if segue.identifier == "Show Result"{
+            
+            // Puede que me haya equivocado al programar
+            if let rvc = segue.destination as? ResultadosViewController{
+                
+                let index = tioSegmentedControl.selectedSegmentIndex
+                
+                if index == -1 {return }
+                
+                let t = tioSegmentedControl.titleForSegment(at: index)
+                rvc.tipoDeTio  = t
+            }
+    }
 
 }
 
+}
